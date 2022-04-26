4ec349ad55eecb8e14597f617c9952addea7d747
PCloud <pcloud63514@gmail.com>
2022-04-26 10:36:18 +0900

    Eureka!

--- applications/app-gateway/src/main/java/com/example/GatewayApplication.java
+++ applications/app-gateway/src/main/java/com/example/GatewayApplication.java
@@ -3,13 +3,11 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableDiscoveryClient
-@EnableJpaAuditing
 @SpringBootApplication
-public class OhLotApplication {
+public class GatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OhLotApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 }
